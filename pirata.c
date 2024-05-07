8788053559967f24d13f9415566a24a1c71bc4c4
maximaldo <130598408+maximaldo@users.noreply.github.com>
2024-05-07 15:40:36 -0300

    hicimos el tablero, falta imprimirlo

--- pirata.c
+++ pirata.c
@@ -5,17 +5,37 @@ void tablero(){
 	int num;
     scanf("%d",&num);
     char tabla[num][num];
-    int pirata = (rand() % num) + 1;
-    int tesoro = (rand() % num) + 1;
+    int pirataf;
+    int piratac;
+    int tesorof;
+    int tesoroc;
     for(int i = 0; i < num;i++){
     	for(int j = 0; j < num;j++){
     		if(i = 0 && j = num - 1 || i = num - 1 && j = 0){
-    		tabla[i - 1][j - 1] = 'B';	
-			}else if(i = )
+    		tabla[i][j] = 'B';	
+			}else if(i == 0 || i == num - 1 || j == 0 || j == num - 1){
+    		tabla[i][j] = 'A';	
+			}else{
+    		tabla[i][j] = '-';
+			}
 		}
 	}
-    
+	do{
+    pirataf = (rand() % (num - 1)) + 1;
+    piratac = (rand() % (num - 1)) + 1;
+    tesorof = (rand() % (num - 1)) + 1;
+	tesoroc = (rand() % (num - 1)) + 1;	
+	} while(pirafaf == tesorof && piratac == tesoroc);
+	tabla[pirataf][piratac] = 'P';	
+    tabla[tesorof]tesoroc] = 'T';	
 }
+
+char imprimir(){
+	
+	
+}
+
+
 
 
 int main(int argc, char *argv[]) {
