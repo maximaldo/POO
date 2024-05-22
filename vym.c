8e0839b095a92ccdabe0bf51fbeb47f7198feebc
maximaldo <130598408+maximaldo@users.noreply.github.com>
2024-05-22 12:53:41 -0300

    Add files via upload

---
+++ vym.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pedilo(){
+	int num[4];
+    printf("Ingrese 4 numeros separados por un espaccio y te dire la medida aritmetica\n");
+    scanf("%d %d %d %d", &num[0], &num[1], &num[2], &num[3]);
+    int sum = (num[0] + num[1] + num[2] + num[3]) / 4;
+    printf("Los numeros ingresados son %d %d %d %d y la media aritmetica es %d",num[0], num[1], num[2], num[3], sum);
+}
+
+void ejer2(){
+	int nums[4];
+	int 4;
+	for(int i = 0; i < 5;i++){
+		printf("Decime el 1 nu")
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	pedilo();
+	return 0;
+}
