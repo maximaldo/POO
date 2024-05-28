be4508d6defb70e768fec83c0e8899ae213fb7e1
maximaldo <130598408+maximaldo@users.noreply.github.com>
2024-05-28 15:42:26 -0300

    desorderar primer tabla sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -10,14 +10,17 @@
 	}
 		
 }*/
-
+	 int random1;
+     int random2;
+     int guardado;
 void desordenar(){
 	int nums[9]= {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	for(int i = 0; i < 50; i++){
 		int random1 = rand() % 9;
      	int random2 = rand() % 9;
-		nums[random1] = nums[random2];
-		nums[random2] = nums[random1];
+        int guardar = nums[random1];
+        nums[random1] = nums[random2];
+        nums[random2] = guardar;
 		
 	}
 	for(int j = 0; j < 9; j++){
